6cd06bc617902065992da74779df5d1fbc26ffe0
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-12-07 14:07:40 -0300

    add: funcion para calcula rlos puntos

--- src/components/ManosJugadores.jsx
+++ src/components/ManosJugadores.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TarjetaImg } from './TarjetaImg';
+import { calcularPuntos } from '../functions/calcularPuntos';
 
 export const ManosJugadores = ({ jugador, deck }) => {
+  const [puntosJugador1, setPuntosJugador1] = useState(0);
+  const [puntosJugador2, setPuntosJugador2] = useState(0);
+
+  useEffect(() => {
+    calcularPuntos(deck, jugador, setPuntosJugador1, setPuntosJugador2);
+  }, [deck, jugador]); 
+
+  
+
   return (
     <section>
       <h2>Jugador {jugador}</h2>
+      <h3>Puntos: {jugador === 1 ? puntosJugador1 : puntosJugador2}</h3>
       <div>
         {deck.map((carta, index) => (
-          <p key={index}>
+          <div key={index}>
             <TarjetaImg carta={carta} />
-          </p>
+          </div>
         ))}
       </div>
     </section>
