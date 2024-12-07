1e01c4687a480be56033751f746f94884667e2e9
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-12-07 16:40:44 -0300

    add: pedir carta, edit : css img-cartas

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import { ManosJugadores } from './components/ManosJugadores'
 function App() {
   const [mazoMezclado, setMazoMezclado] = useState([])
   const [deck, setDeck] = useState([])
-  
+  const [cartasJugador1, setCartasJugador1] = useState([])
+  const [cartasJugador2, setCartasJugador2] = useState([])  
 
   const nuevoJuego = () => {
     fetchMazo(setMazoMezclado)
+    repartirCartas()
   }
 
   useEffect(() => {
@@ -17,8 +19,17 @@ function App() {
 
   const repartirCartas = () => {
     const cartas = mazoMezclado.slice(0, 4)
+    setCartasJugador1(cartas.slice(0, 2))
+    setCartasJugador2(cartas.slice(2, 4))
     setDeck(cartas)
     setMazoMezclado(mazoMezclado.slice(4))
+  }
+
+  const pedirCarta = () => {
+    const carta = mazoMezclado.slice(0, 1) 
+    setCartasJugador1([...cartasJugador1, ...carta])
+    setDeck([...deck, ...carta]) 
+    setMazoMezclado(mazoMezclado.slice(1))
   }
 
 
@@ -26,11 +37,15 @@ function App() {
     <div className='game-section'>
       <h1>Blackjack</h1>
       <div className='cartas-section'>
-        <ManosJugadores jugador={1} deck={deck.slice(0, 2)} />
-        <ManosJugadores jugador={2} deck={deck.slice(2, 4)} />
+        <ManosJugadores jugador={1} deck={cartasJugador1} />
+        <ManosJugadores jugador={2} deck={cartasJugador2} />
       </div>
-      <button onClick={nuevoJuego}>Nuevo Juego</button>
-      <button onClick={repartirCartas}>Repartir</button>
+      <div className='game-buttons'>
+        <button onClick={nuevoJuego}>Nuevo Juego</button>
+        <button onClick={repartirCartas}>Repartir</button>
+        <button onClick={pedirCarta} >Pedir</button>
+
+      </div>
 
     </div>
   )
